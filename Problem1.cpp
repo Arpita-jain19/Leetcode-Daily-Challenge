7d320812a929d9eb61ccc46ff4f7824c0a03ddfa
Arpita-jain19 <jainarpita56@gmail.com>
2023-12-30 10:11:45 +0530

    first commit

---
+++ Problem1.cpp
@@ -0,0 +1,37 @@
+/*You are given an array of strings words (0-indexed).
+
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.*/
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        if (words.size() == 1) {
+            return true;
+        }
+
+        int totalCharCount = 0;
+        for (const string& s : words) {
+            totalCharCount += s.length();
+        }
+
+        if (totalCharCount % words.size() != 0) {
+            return false;
+        }
+
+        vector<int> myMap(26, 0);
+        for (const string& s : words) {
+            for (char c : s) {
+                myMap[c - 'a']++;
+            }
+        }
+
+        for (int i : myMap) {
+            if (i % words.size() != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
